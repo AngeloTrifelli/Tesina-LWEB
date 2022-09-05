69985804ba7705c85511442a1092abc92a22abb2
AngeloTrifelli <103106138+AngeloTrifelli@users.noreply.github.com>
2022-09-05 20:35:52 +0200

    Add files via upload

---
+++ PHP/datiPersonali.php
@@ -0,0 +1,110 @@
+<?php
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>Dati personali</title>
+
+    <style>
+        <?php include "../CSS/datiPersonali.css" ?>
+    </style>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato" />
+
+
+
+</head>
+
+
+<body>
+    <div class="top">
+        <div class="topLeft">
+            <a href="test">TORNA INDIETRO</a>    
+        </div>
+        <h1 class="alignCenter">Ciao Angelo !</h1>
+        <div style="width: 18.5%;"></div>
+    </div>
+
+    
+    <h3 class="titoloImportante alignCenter">I TUOI DATI:</h3>
+    <div class="mainContainer">
+        <div class="containerDati">
+            <div class="datiUtenteSx">
+                <div>
+                    <strong>Nome:</strong> Angelo
+                </div>
+                <div>
+                    <strong>Cognome:</strong> Trifelli
+                </div>
+                <div>
+                    <strong>Codice fiscale:</strong> TRFNGL00T25A341U
+                </div>
+                <div>
+                    <strong>Data di nascita:</strong> 25-12-2000
+                </div>
+                <div>
+                    <strong>Indirizzo:</strong> via Francesco Cilea 7
+                </div>
+            </div>
+            <div class="datiUtenteDx">
+                <div>
+                    <strong>Telefono:</strong> 3490750745
+                </div>
+                <div>
+                    <strong>Email:</strong> trifelli.angelo@outlook.it
+                </div>
+                <div>
+                    <strong>Numero carta:</strong> 0000-0000-0000-0000
+                </div>
+                <div>
+                    <strong>Username:</strong> AngeloTrifelli
+                </div>
+                <div>
+                    <strong>Password:</strong> FioreDiZucca
+                </div>
+            </div>
+        </div>
+        <div class="datiUtenteCentro">
+            <strong>Crediti: 0</strong>
+            <strong>Somma giudizi ricevuti: 0</strong>
+        </div>
+    </div>
+   
+
+    <h3 class="titoloImportante alignCenter">IL TUO SOGGIORNO:</h3>
+        <table>
+            <tr>
+                <td>
+                    Numero Camera<br />
+                    110
+                </td>
+                <td>
+                    Tipo<br />
+                    Standard Doppia
+                </td>
+                <td>
+                    Stato soggiorno<br />
+                    Approvato
+                </td>
+                <td>
+                    Inizio<br />
+                    28-10-2022
+                </td>
+                <td>
+                    Fine<br />
+                    31-10-2022
+                </td>
+            </tr>
+        </table>
+
+    <h3 class="titoloImportante alignCenter">SOGGIORNI PASSATI:</h3>
+      
+    
+        
+</body>
+
+
+</html>
