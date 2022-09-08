f7143cea516e19c3c2229828a917937b0cfe6b9b
AngeloTrifelli <103106138+AngeloTrifelli@users.noreply.github.com>
2022-09-09 01:04:21 +0200

    Aggiunto file per le funzioni MODIFICA di PHP

---
+++ PHP/funzioniModificaPHP.php
@@ -0,0 +1,17 @@
+<?php
+// FILE CHE CONTIENE TUTTE E SOLO LE FUNZIONI PHP PER MODIFICARE I FILE XML
+
+function modificaDatiUtente ($datoDaModificare , $arrayValore){
+    if($datoDaModificare != "codFisc" && $datoDaModificare != "username" && $datoDaModificare != "password"){
+        // DA IMPLEMENTARE
+    }
+    else{
+        // DA IMPLEMENTARE (controlli per codFisc , username e password vecchia)
+    }
+}
+
+
+
+
+
+?>
