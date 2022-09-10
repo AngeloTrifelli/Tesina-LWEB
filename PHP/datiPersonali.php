91f12274f4855d637bf566d4d52ae89229456f5c
AngeloTrifelli <trifelli.1920939@studenti.uniroma1.it>
2022-09-11 01:48:24 +0200

    Modifica dati cliente e soggiorno attivo
    Ora i dati del cliente e del soggiorno attivo non sono più statici

--- PHP/datiPersonali.php
+++ PHP/datiPersonali.php
@@ -1,4 +1,31 @@
 <?php
+    require_once('funzioniGetPHP.php');
+
+    session_start();
+    if(isset($_SESSION['codFiscUtenteLoggato'])){
+        $cliente = getDatiCliente($_SESSION['codFiscUtenteLoggato']);
+    }
+    else{
+        header('Location: intro.php');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 
@@ -21,7 +48,7 @@
         <div class="topLeft">
             <a href="./areaUtente.php">TORNA INDIETRO</a>    
         </div>
-        <h1 class="alignCenter">Ciao Angelo !</h1>
+        <h1 class="alignCenter">Ciao <?php echo $cliente['nome'];?>!</h1>
         <div style="width: 18.5%;"></div>
     </div>
 
@@ -32,31 +59,31 @@
             <div class="containerDati">
                 <div class="datiUtenteSx">
                     <div>
-                        <strong>Nome:</strong> Angelo 
+                        <strong>Nome:</strong> <?php echo $cliente['nome'];?>
                         <button class="button" type="submit" name="nome" value="nome">
                             <img class="immagine" src="../img/edit.png" />
                         </button>
                     </div>
                     <div>
-                        <strong>Cognome:</strong> Trifelli
+                        <strong>Cognome:</strong> <?php echo $cliente['cognome'];?>
                         <button class="button" type="submit" name="cognome" value="cognome">
                             <img class="immagine" src="../img/edit.png" />
                         </button>
                     </div>
                     <div>
-                        <strong>Codice fiscale:</strong> TRFNGL00T25A341U 
+                        <strong>Codice fiscale:</strong> <?php echo $cliente['codFisc'];?>
                         <button class="button" type="submit" name="codFisc" value="codFisc">
                             <img class="immagine" src="../img/edit.png" />
                         </button>
                     </div>
                     <div>
-                        <strong>Data di nascita:</strong> 25-12-2000
+                        <strong>Data di nascita:</strong> <?php echo $cliente['dataDiNascita'];?>
                         <button class="button" type="submit" name="dataDiNascita" value="dataDiNascita">
                             <img class="immagine" src="../img/edit.png" />
                         </button>
                     </div>
                     <div>
-                        <strong>Indirizzo:</strong> via Francesco Cilea 7 
+                        <strong>Indirizzo:</strong> <?php echo $cliente['indirizzo'];?>
                         <button class="button" type="submit" name="indirizzo" value="indirizzo">
                             <img class="immagine" src="../img/edit.png" />
                         </button>
@@ -64,25 +91,25 @@
                 </div>
                 <div class="datiUtenteDx">
                     <div>
-                        <strong>Telefono:</strong> 3490750745
+                        <strong>Telefono:</strong> <?php echo $cliente['telefono'];?>
                         <button class="button" type="submit" name="telefono" value="telefono">
                             <img class="immagine" src="../img/edit.png" />
                         </button>
                     </div>
                     <div>
-                        <strong>Email:</strong> trifelli.angelo@outlook.it
+                        <strong>Email:</strong> <?php echo $cliente['email'];?>
                         <button class="button" type="submit" name="email" value="email">
                             <img class="immagine" src="../img/edit.png" />
                         </button>
                     </div>
                     <div>
-                        <strong>Numero carta:</strong> 0000-0000-0000-0000
+                        <strong>Numero carta:</strong> <?php echo $cliente['numeroCarta'];?>
                         <button class="button" type="submit" name="numeroCarta" value="numeroCarta">
                             <img class="immagine" src="../img/edit.png" />
                         </button>
                     </div>
                     <div>
-                        <strong>Username:</strong> AngeloTrifelli
+                        <strong>Username:</strong> <?php echo $cliente['username'];?>
                         <button class="button" type="submit" name="username" value="username">
                             <img class="immagine" src="../img/edit.png" />
                         </button>
@@ -97,41 +124,75 @@
             </div>
         </form>
         <div class="datiUtenteCentro">
-            <strong>Crediti: 0</strong>
-            <strong>Somma giudizi ricevuti: 0</strong>
+            <strong>Crediti: <?php echo $cliente['crediti'];?> </strong>
+            <strong>Somma giudizi ricevuti: <?php echo $cliente['sommaGiudizi'];?></strong>
         </div>
     </div>
    
 
     <h3 class="titoloImportante alignCenter">IL TUO SOGGIORNO:</h3>
-    <div class="mainContainer">
-        <table class="prenotazione alignCenter" align="center">
-            <tr>
-                <td>
-                    <strong>Numero Camera</strong><br />
-                    110
-                </td>
-                <td>
-                    <strong>Tipo</strong><br />
-                    Standard Doppia
-                </td>
-                <td>
-                    <strong>Stato soggiorno</strong><br />
-                    <span class="success">Approvato</span>
-                </td>
-                <td>
-                    <strong>Inizio</strong><br />
-                    28-10-2022
-                </td>
-                <td>
-                    <strong>Fine</strong><br />
-                    31-10-2022
-                </td>
-            </tr>
-        </table>
-    </div>
-    <!-- <p class="alignCenter scrittaCentrale">Non è stata trovata una prenotazione attiva...</p> -->
-    <!-- Con il php poi controlla quando mostrare questa scritta o meno -->  
+
+    <?php 
+        if($_SESSION['soggiornoAttivo'] != "null"){
+            $temp = $_SESSION['soggiornoAttivo'];
+    ?>
+        <div class="mainContainer">
+            <table class="prenotazione alignCenter" align="center">
+                <tr>
+                    <td>
+                        <strong>Numero Camera</strong><br />
+                        <?php echo $temp['numeroCamera'];?>
+                    </td>
+                    <td>
+                        <strong>Tipo</strong><br />
+                        <?php echo $temp['tipoCamera'];?>
+                    </td>
+                    <td>
+                        <strong>Stato soggiorno</strong><br />
+                        <?php
+                            if($temp['statoSoggiorno'] == "Approvato"){
+                        ?>   
+                                <span class="success"><?php echo $temp['statoSoggiorno']?></span>
+                        <?php        
+                            }
+                            else{
+                        ?>
+                                <span class="waiting"><?php echo $temp['statoSoggiorno']?></span>
+                        <?php
+                            }
+                        ?>
+                        
+                    </td>
+                    <td>
+                        <strong>Inizio</strong><br />
+                        <?php
+                            $stringaData = $temp['dataArrivo'];
+                            $giorno = substr($stringaData, 8,2);       
+                            $mese = substr($stringaData,5,2 );
+                            $anno = substr($stringaData,0,4 );
+                            echo $giorno."-".$mese."-".$anno;
+                        ?>
+                    </td>
+                    <td>
+                        <strong>Fine</strong><br />
+                        <?php
+                            $stringaData = $temp['dataPartenza'];
+                            $giorno = substr($stringaData, 8,2);       
+                            $mese = substr($stringaData,5,2 );
+                            $anno = substr($stringaData,0,4 );
+                            echo $giorno."-".$mese."-".$anno;
+                        ?>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    <?php
+        }
+        else{
+            echo '<p class="alignCenter scrittaCentrale">Non è stata trovata una prenotazione attiva...</p>';
+        }
+    ?>
+
 
     <h3 class="titoloImportante alignCenter">SOGGIORNI PASSATI:</h3>
     <div class="mainContainer">
