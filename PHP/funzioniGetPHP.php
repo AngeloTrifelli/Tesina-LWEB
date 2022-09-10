206b5d94461080dda9293dfaacbadd908c4f1523
AngeloTrifelli <103106138+AngeloTrifelli@users.noreply.github.com>
2022-09-10 10:27:45 +0200

    Update funzioniGetPHP.php

--- PHP/funzioniGetPHP.php
+++ PHP/funzioniGetPHP.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<?php
-// FILE CHE CONTIENE TUTTE E SOLO LE FUNZIONI PER ESTRARRE DATI DA I FILE XML
-
-
-// Funzione per verificare se il cliente che ha effettuato il login ha un soggiorno attivo al momento (cioè con pagamento approvato)
-
-function getSoggiornoAttivo($codFiscCliente){
-    // DA IMPLEMENTARE
-}
-
-
-
-
-
-
-=======
 <?php
 // FILE CHE CONTIENE TUTTE E SOLO LE FUNZIONI PER ESTRARRE DATI DA I FILE XML
 
@@ -44,5 +27,4 @@ function getSoggiornoAttivo($codFiscCliente){
 
 
 
->>>>>>> master
 ?>
