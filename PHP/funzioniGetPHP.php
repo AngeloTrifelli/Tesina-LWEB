7dfa33d0c9c296e21f3ac0e958c24af13e31b7bc
SimoneAmoriello <103106135+SimoneAmoriello@users.noreply.github.com>
2022-09-10 19:32:58 +0200

    Aggiunta funzioni per  CodFisc e Username

--- PHP/funzioniGetPHP.php
+++ PHP/funzioniGetPHP.php
@@ -22,6 +22,67 @@ function getSoggiornoAttivo($codFiscCliente){
     }
 }
 
+//Funzione per verificare se il cliente con codice fiscale passato come parametro è presente nel file XML
+
+function getCodFisc($codFiscCliente){
+    $trovato="False";
+    $xmlStringClienti = "";
+            foreach(file("../XML/Clienti.xml") as $node){
+                $xmlStringClienti .= trim($node);
+            }
+
+            $docClienti = new DOMDocument();
+            $docClienti->loadXML($xmlStringClienti);
+
+            $listaClienti = $docClienti->documentElement->childNodes;
+            $i = 0;
+            while($i < $listaClienti->length && $trovato == "False"){         
+                $cliente = $listaClienti->item($i);
+                $codFiscale = $cliente->getAttribute("codFisc");
+                if($codFiscale == $codFiscCliente){
+                    $duplicato = "True";
+                }
+                else{
+                    $i++;
+                }
+            }
+        return $trovato;
+}
+
+//Funzione per verificare se il cliente con l'username passato come parametro è presente nel file XML
+
+function getUsername($usernameCliente){
+    $trovato="False";
+    $xmlStringClienti = "";
+
+    foreach(file("../XML/Clienti.xml") as $node){
+
+        $xmlStringClienti .= trim($node);
+
+    }
+            
+            $docClienti = new DOMDocument();
+            $docClienti->loadXML($xmlStringClienti);
+
+            $listaClienti = $docClienti->documentElement->childNodes;
+            $i = 0;
+            while($i < $listaClienti->length && $trovato == "False"){          //Controllo se un utente con tale username è già registrato
+                $cliente = $listaClienti->item($i);
+                $elemCredenziali= $cliente->getElementsByTagName("credenziali")->item(0);
+                $testoUsername=$elemCredenziali->firstChild->textContent;
+                if($testoUsername == $usernameCliente){
+                    $trovato = "True";
+                }
+                else{
+                    $i++;
+                }
+            }
+
+            return $trovato;
+
+}
+
+
 
 
 
