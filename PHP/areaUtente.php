abba2919a9402934e968a575cdccf4fde93c33d4
SimoneAmoriello <amoriello.1898815@studenti.uniroma1.it>
2022-09-23 12:16:15 +0200

    Modifiche areaUtente.php Semplice inserimento di collegamenti con le pagine dell'admin.

--- PHP/areaUtente.php
+++ PHP/areaUtente.php
@@ -92,7 +92,7 @@
                         break;
                     case "Concierge":
                         echo '
-                        <a class="item" href="#">HOME</a>
+                        <a class="item" href="./intro.php">HOME</a>
                         <br /> 
                           
                         <a class="item" href="#">MODIFICA MENU RISTORANTE</a>
@@ -112,7 +112,7 @@
                         break;
                     case "Admin":
                         echo '
-                        <a class="item" href="#">HOME</a>
+                        <a class="item" href="./intro.php">HOME</a>
                         <br /> 
                           
                         <a class="item" href="#">MODIFICA MENU RISTORANTE</a>
@@ -134,13 +134,13 @@
                         <br />
                         <br />
 
-                        <a class="item" href="#">CATEGORIE</a>
+                        <a class="item" href="./categorie.php">CATEGORIE</a>
                         <br />
                         
-                        <a class="item" href="#">PAGAMENTI CLIENTI</a>
+                        <a class="item" href="./pagamentiClienti.php">PAGAMENTI CLIENTI</a>
                         <br />
 
-                        <a class="item" href="#">VISUALIZZA CLIENTI</a>
+                        <a class="item" href="./visualizzaClienti.php">VISUALIZZA CLIENTI</a>
                         <br />';
                         break;
                 }
