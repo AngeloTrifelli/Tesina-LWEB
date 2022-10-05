a4e551a68ff07d441b19d5ecd17536f96e7824c0
SimoneAmoriello <amoriello.1898815@studenti.uniroma1.it>
2022-10-05 15:32:01 +0200

    Aggiunta faq Aggiunte pagine faq

---
+++ PHP/modificaFaq.php
@@ -0,0 +1,86 @@
+<?php
+    require_once("funzioniGetPHP.php");
+    require_once("funzioniModificaPHP.php");
+
+    session_start();
+
+    if(!isset($_SESSION['codFiscUtenteLoggato'])){
+        if(!isset($_SESSION['loginType'])){
+            header('Location: intro.php');
+        }
+    }
+    else{
+        header('Location: areaUtente.php');
+    }
+
+    $datiNonInseriti="False";
+    if(isset($_POST['ANNULLA']) || isset($_POST['CONFERMA'])){
+        if(isset($_POST['ANNULLA'])){
+            unset($_SESSION['idFaq']);
+            header('Location: faq.php');
+            exit();
+        }
+        else{
+            modificaFaq($_SESSION['idFaq'],$_POST['risposta']);
+            unset($_SESSION['idFaq']);
+            header('Location: faq.php');
+            exit();
+        }
+    }
+
+    $faq=getFAQ($_SESSION['idFaq']);
+
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <title>Modifica menu</title>
+
+        <style type="text/css">
+            <?php include "../CSS/modificaFaq.css" ?>
+        </style>
+
+        <link href="https://code.jquery.com/ui/1.13.2/themes/blitzer/jquery-ui.css" rel="stylesheet"/>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script type="text/javascript" src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
+
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css" />
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&amp;display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&amp;display=swap" />
+    </head>
+
+    <body>
+
+        <div class="containerCentrale">
+            <h1>MODIFICA FAQ</h1>
+            <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post"  >
+            
+            <div class="mainArea">
+                    <div class="zonaSuperiore">
+                        <div class="zonaSx">
+                            <span class="item">Testo della domanda:</span>
+                            <p><?php echo $faq['testoDomanda']; ?></p>
+                        </div>
+                        <div class="zonaDx">
+
+                            <span class="item">Modifica il testo della risposta:</span>
+                            <textarea type="text" class="textInput note" name="risposta"><?php echo $faq['testoRisposta']; ?></textarea>    
+
+                        </div>
+                    </div>
+
+                    <div class="zonaBottoni">
+                        <input type="submit" class="button" name="ANNULLA" value="ANNULLA" />
+                        <input type="submit" class="button" name="CONFERMA" value="CONFERMA" />
+                    </div>
+            </div>
+            </form>
+        </div>
+    </body>
+</html>
